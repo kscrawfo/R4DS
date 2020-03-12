2d7da78a160f0534fb66edad58079c36b6b7582b
Kyle Crawford <kyle.s.crawford@gmail.com>
2020-03-11 18:44:13 -0700

    completed ch 12 factors

--- R/R for Data Science (Chapter 12 -- forcats).R
+++ R/R for Data Science (Chapter 12 -- forcats).R
@@ -137,3 +137,152 @@ by_age %>%
   ggplot(aes(age, prop, color = fct_reorder2(marital, age, prop))) +
   geom_line() +
   labs(color = "marital")
+
+gss_cat %>%
+  mutate(marital = marital %>% fct_infreq() %>% fct_rev()) %>%
+  ggplot(aes(marital)) +
+  geom_bar()
+
+?fct_rev
+
+
+### 12.2 Exercises
+## 1
+gss_cat %>%
+  ggplot(aes(tvhours)) +
+  geom_bar()
+
+# seems reasonable to me. mean or median would probably both be fine in this instance
+mean(gss_cat$tvhours, na.rm = TRUE)
+median(gss_cat$tvhours, na.rm = TRUE)
+
+## 2
+colnames(gss_cat)
+
+levels(gss_cat$marital)
+# arbitrary
+
+levels(gss_cat$race)
+# arbitrary
+
+levels(gss_cat$rincome)
+# principled
+
+levels(gss_cat$partyid)
+# principled
+
+levels(gss_cat$relig)
+# arbitrary
+
+levels(gss_cat$denom)
+# arbitrary
+
+## 3
+
+rincome <- gss_cat %>%
+  group_by(rincome) %>%
+  summarise(
+    age = mean(age, na.rm = TRUE),
+    tvhours = mean(tvhours, na.rm = TRUE),
+    n = n()
+  )
+
+rincome %>%
+  ggplot(aes(age, rincome)) +
+  geom_point()
+
+rincome %>%
+  mutate(rincome = fct_relevel(rincome, "Not applicable")) %>%
+  ggplot(aes(age, rincome)) +
+  geom_point()
+
+?fct_relevel
+# because this explicity puts "Not applicable" as the first level in factor
+
+levels(rincome$rincome)
+levels(fct_relevel(rincome$rincome, "Not applicable"))
+
+### 12.3 Modifying factor levels
+
+gss_cat %>% count(partyid)
+
+
+gss_cat %>%
+  mutate(partyid = fct_recode(partyid,
+                              "Republican, strong"    = "Strong republican",
+                              "Republican, weak"      = "Not str republican",
+                              "Independent, near rep" = "Ind,near rep",
+                              "Independent, near dem" = "Ind,near dem",
+                              "Democrat, weak"        = "Not str democrat",
+                              "Democrat, strong"      = "Strong democrat"
+  )
+         ) %>%
+  count(partyid)
+
+gss_cat %>%
+  mutate(partyid = fct_recode(partyid,
+                              "Republican, strong"    = "Strong republican",
+                              "Republican, weak"      = "Not str republican",
+                              "Independent, near rep" = "Ind,near rep",
+                              "Independent, near dem" = "Ind,near dem",
+                              "Democrat, weak"        = "Not str democrat",
+                              "Democrat, strong"      = "Strong democrat",
+                              "Other"                 = "No answer",
+                              "Other"                 = "Don't know",
+                              "Other"                 = "Other party"
+  )) %>%
+  count(partyid)
+
+gss_cat %>%
+  mutate(partyid = fct_collapse(partyid,
+                                other = c("No answer", "Don't know", "Other party"),
+                                rep = c("Strong republican", "Not str republican"),
+                                dem = c("Strong democrat", "Not str democrat"),
+                                ind = c("Ind,near rep", "Independent", "Ind,near dem")
+                                )
+         ) %>%
+  count(partyid)
+
+gss_cat %>%
+  mutate(relig = fct_lump(relig)) %>%
+  count(relig)
+
+gss_cat %>%
+  mutate(relig = fct_lump(relig, n = 5)) %>%
+  count(relig)
+
+### 12.3 exercises
+## 1
+unique(gss_cat$year)
+
+gss_cat %>%
+  mutate(partyid = fct_collapse(partyid,
+                                other = c("No answer", "Don't know", "Other party"),
+                                rep = c("Strong republican", "Not str republican"),
+                                dem = c("Strong democrat", "Not str democrat"),
+                                ind = c("Ind,near rep", "Independent", "Ind,near dem")
+  )
+  ) %>%
+  count(year, partyid) %>%
+  group_by(year) %>%
+  mutate(prop = n / sum(n)) %>%
+  ggplot(aes(year, prop, color = partyid)) +
+  geom_line()
+
+# republicans seem to be downtrending
+# dems relatively stable
+# indepts (and to some extent others) going up
+
+## 2
+levels(gss_cat$rincome)
+
+gss_cat %>%
+  mutate(rincome = fct_collapse(rincome,
+                                vlow = c("Lt $1000", "$1000 to 2999", "$3000 to 3999", "$4000 to 4999"),
+                                low = c("$5000 to 5999", "$6000 to 6999", "$7000 to 7999", "$8000 to 9999"),
+                                mid = c("$10000 - 14999", "$15000 - 19999"),
+                                high = c("$20000 - 24999", "$25000 or more"),
+                                other = c("No answer", "Don't know", "Refused", "Not applicable")
+                                )
+  ) %>%
+  count(rincome)
